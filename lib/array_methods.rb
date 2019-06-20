01464aadc0189b7d124d0149f41a384dba9ca276
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-06-20 20:23:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.find_element_index(value_to_find)
 end
 
 def find_max_value(array)
@@ -9,3 +9,7 @@ end
 def find_min_value(array)
   # Add your solution here
 end
+
+a = ["S", "H"]
+
+puts find_element_index(a, "H")
